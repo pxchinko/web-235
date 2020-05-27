da8dbb215a7c821d0e92928797b3b254e7ee68d3
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2020-05-26 19:37:30 -0700

    New Assignments

---
+++ assignment-1.4.js
@@ -0,0 +1,23 @@
+//List of Variable Declaration
+var firstName;
+var lastName
+var age
+var street
+var city
+var zipCode
+
+//List of Variable Assignments
+firstName = "Douglas";
+lastName = "Jenkins";
+age = "25";
+street = "4078 Midway Ave";
+city = "Bakersfield";
+zipCode = "45768";
+
+//Output of the Variables
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
---
+++ hello-world.js
@@ -0,0 +1,11 @@
+/* ============================================ 
+; Title: Hello World
+; Author: Professor Krasso
+; Date: 25 May 2020
+; Modified By: Douglas Jenkins
+; Description: This program demonstrates the console.log
+; and submits hello world. 
+;=========================================== */
+
+//Console.log to have Hello World shown
+console.log("Hello World, my name is Douglas Jenkins");
