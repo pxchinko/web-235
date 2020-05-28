43862c9e3ce9ff84856c270b278b8928151f15cd
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2020-05-27 20:46:47 -0700

    JavaScript Assignment

---
+++ jenkins-assignment-1.4.js
@@ -0,0 +1,31 @@
+/* ============================================ 
+; Title: Hello World
+; Author: Professor Krasso
+; Date: 25 May 2020
+; Modified By: Douglas Jenkins
+; Description: Variable Declaration and Assignments. 
+;=========================================== */
+
+//List of Variable Declaration
+var firstName;
+var lastName;
+var age;
+var street;
+var city;
+var zipCode;
+
+//List of Variable Assignments
+firstName = "Douglas";
+lastName = "Jenkins";
+age = "25";
+street = "4078 Midway Ave";
+city = "Bakersfield";
+zipCode = "45768";
+
+//Output of the Variables
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
