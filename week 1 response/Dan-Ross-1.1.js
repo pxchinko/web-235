56f8604bb2233fea94c3590c42417cafa4c07bd0
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2020-05-30 20:20:27 -0700

    Updated

---
+++ week 1 response/Dan-Ross-1.1.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Professor Krasso
+; Date: 27 May 2020
+; Modified By: Dan Ross
+; Description: Values and variables with 
+; at least two errors
+;===========================================
+*/
+
+//There are at least 4 errors in this program!
+
+var normal = 9;
+var discount = 2;
+
+//subtracts discount from default price
+var total = normal - discount;
+
+//outputs total price after discount
+console.log("The total is " + total);
---
+++ week 1 response/James-Liverman-1.1.js
@@ -0,0 +1,18 @@
+/* ============================================ 
+; Title: Discussion Board Mistake
+;Author: Professor Krasso
+;Date: 26 May 2020
+;Modified By: James Liverman
+;Description: There are mistakes below
+=========================================== */
+//List of Variable Declaration
+var movieName;
+var type;
+
+//This variable has a mistake
+movieName = "Superman";
+type = "Action";
+
+//The output has 2 mistakes
+console.log(movieName);
+console.log(type);
