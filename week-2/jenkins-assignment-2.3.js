533ae3c56ddb9910a32aba5e232781ecaca6f601
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2020-06-03 17:53:20 -0700

    Initial commit

---
+++ week-2/jenkins-assignment-2.3.js
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  jenkins-assignment-2.3
+; Author: Professor Krasso
+; Modified By: Douglas Jenkins
+; Date: 3 June 2020
+; Description: Displays a name with function
+;===========================================
+*/
+
+// function properties
+myName.Douglas = "Douglas";
+
+// function
+function myName() {
+  return myName.Douglas;
+}
+
+// output. '\n' creates new line
+console.log('\n')
+console.log("Hello " + myName() + " Jenkins");
---
+++ week-2/jenkins-assignment-2.4.js
@@ -0,0 +1,44 @@
+/*
+============================================
+; Title:  jenkins-assignment-2.4
+; Author: Professor Krasso
+; Modified By: Douglas Jenkins
+; Date: 31 May 2020
+; Description: List of functions
+;===========================================
+*/
+
+// names
+function fullName(firstName, lastName){
+  return firstName + lastName;
+}
+
+// current day
+function dateWriter(year, month, day){
+return year + month + day;
+}
+
+// Format Number
+function formatNumber(number, numOfFixedPositions){
+return number + numOfFixedPositions.toFixed(5);
+}
+
+
+// Return Int
+function convertToInt(){
+return parseInt("15.99");
+}
+
+// Return Float
+function convertToFloat(){
+return parseFloat("26.33");
+}
+
+
+
+// output
+console.log( 'Hello my name is ' + fullName("Douglas", " Jenkins"));
+console.log( 'Todays date is ' + dateWriter(2020, " June ", 03));
+console.log(formatNumber(15 , 5));
+console.log(convertToInt());
+console.log(convertToFloat());
